8404da55f82096cb8b17e87155a23fce6f77e0a8
liammtech <liammtech@gmail.com>
2024-10-28 16:51:35 +0000

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    strArray = str.split("");
+    revArray = [];
+    for (let char in strArray) {
+        revArray.unshift(strArray[char]);
+    };
+    return revArray.join("");
 };
 
 // Do not edit below this line
