f10d12a3dc912411c07ebf12810db0d201d6c559
Liam <liammtech@gmail.com>
2025-05-24 13:14:52 +0100

    Finish 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(num) {
+    if (
+        (num % 400 === 0) ||
+        (num % 4 === 0 && num % 100 != 0)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
