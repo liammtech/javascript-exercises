122741a2e5feb8e3dd46c3cea648e93e750205f4
Liam <liammtech@gmail.com>
2025-05-25 19:38:08 +0100

    Finish 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function(idx) {
+    let arr = [0, 1];
+    for (i = 2; i < idx; i++) {
+        arr.push(arr[i-1] + arr[i-2])
+    }
+    return arr;
 };
 
 // Do not edit below this line
