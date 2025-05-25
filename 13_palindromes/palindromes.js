57070aae5c3aadbb4a7de365eeef0ee0b77210f1
Liam <liammtech@gmail.com>
2025-05-25 19:07:46 +0100

    Finish 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    arr = str
+        .toLowerCase()
+        .split('')
+        .filter(char => /[a-z0-9]/.test(char));
+    
+    //console.log(`arr: ${arr} ${typeof arr}`)
 
+    revArr = [...arr].reverse()
+    //console.log(`arr after revArr: ${arr} ${typeof arr}`)
+    //console.log(`revArr: ${revArr} ${typeof revArr}`)
+
+    forwardStr = arr.join('');
+    //console.log(`forwardStr: ${forwardStr} ${typeof forwardStr}`)
+
+    revStr = revArr.join('');
+    //console.log(`revStr: ${revStr} ${typeof revStr}`)
+
+    return forwardStr === revStr;
 };
 
 // Do not edit below this line
