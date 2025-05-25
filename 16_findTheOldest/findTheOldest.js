79d941ec034914150ec3b28e07c0666ee60e64b2
Liam <liammtech@gmail.com>
2025-05-25 20:11:13 +0100

    16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    for (item of arr) {
+        item.age = (item.yearOfDeath || (new Date().getFullYear())) - item.yearOfBirth;
+        console.log(item.yearOfDeath);
+    }
+    arr.sort((a, b) => a.age - b.age);
+    return arr[arr.length - 1];
 };
 
 // Do not edit below this line
